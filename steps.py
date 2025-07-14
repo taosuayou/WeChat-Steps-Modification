613fe7c8dd3fa41a2177f2c00ebe2a19777ed705
taosuayou <102899873+taosuayou@users.noreply.github.com>
2025-07-14 15:57:53 +0800

    Update steps.py

--- steps.py
+++ steps.py
@@ -43,8 +43,8 @@ def modify_steps(account, password, min_steps, max_steps, attempts=3, timeout=20
 
 # 主程序
 def main():
-    min_steps = 50000
-    max_steps = 80000
+    min_steps = 2000
+    max_steps = 6000
 
     for account, password in account_password_pairs:
         result = modify_steps(account, password, min_steps, max_steps)
